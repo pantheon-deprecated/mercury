273767cfc976e20bbbdd7a1f0bac01b718a2ba35
Greg Coit <greg@chapterthree.com>
2009-11-14 01:42:49 +0000

    removed defined mysql settings- they get set when pressflow is installed

--- jaunty/conf/settings.php
+++ jaunty/conf/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysqli://root:@localhost/pressflow';
+$db_url = 'mysql://username:password@localhost/databasename';
 $db_prefix = '';
 
 /**
