d0fdfd7e5f97919ef5d179cc67fb4f739663b439
Greg Coit <greg@chapterthree.com>
2009-11-17 23:10:29 +0000

    adjusting script to use symlinks

--- jaunty/conf/init.sh
+++ jaunty/conf/init.sh
@@ -8,25 +8,29 @@ exec &> /etc/mercury/bootlog
 
 cd /var/www/; bzr merge --force
 
-# Move mysql to /mnt
-# TODO: this should be EBS'ed
-mkdir /mnt/tmp
-chown mysql:mysql /mnt/tmp/
-chmod 777 /mnt/tmp
-rsync -a /var/lib/mysql /mnt
-/etc/init.d/mysql restart
-# TODO: clean up vestigal /var/lib/mysql
-    
-# Move varnish to /mnt
-rsync -a /var/lib/varnish /mnt
-/etc/init.d/varnish restart
-# TODO: clean up vistigal /var/lib/varnish
+# Move mysql and varnish to /mnt
+# TODO support for EBS and RDS
+/etc/init.d/mysql stop
+/etc/init.d/varnish stop
+mkdir -p /mnt/mysql/tmp
+chown mysql:mysql /mnt/mysql/
+chmod 777 /mnt/mysql/tmp
+mkdir /mnt/varnish
+mv /var/log/mysql /mnt/mysql/log
+mv /var/lib/mysql /mnt/mysql/lib
+mv /var/lib/varnish /mnt/varnish/lib
+sed --in-place=.bak s*/tmp*/mnt/mysql/tmp* /etc/mysql/my.cnf
+ln -s /mnt/mysql/log /var/log/mysql
+ln -s /mnt/mysql/lib /var/lib/mysql
+ln -s /mnt/varnish/lib /var/lib/varnish
+/etc/init.d/mysql start
+/etc/init.d/varnish start
 
+# Update packages
 apt-get update   
 apt-get -y upgrade
     
 # Set up postfix
-
 HOSTNAME=$(/usr/local/bin/ec2-metadata -p | sed 's/public-hostname: //')
 echo $HOSTNAME > /etc/mailname
 postconf -e 'myhostname = $hostname'
