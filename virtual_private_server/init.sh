743c4279eba2cdf67c328fbb3ca0f72eae2dd2f1
Greg Coit <greg@chapterthree.com>
2010-02-03 20:15:29 +0000

    unified init.sh scripts

--- virtual_private_server/init.sh
+++ virtual_private_server/init.sh
@@ -19,6 +19,8 @@ mkdir /mnt/varnish
 mv /var/log/mysql /mnt/mysql/log
 mv /var/lib/mysql /mnt/mysql/lib
 mv /var/lib/varnish /mnt/varnish/lib
+mkdir /mnt/varnish/lib/pressflow
+chown varnish:varnish /mnt/varnish/lib/pressflow/
 sed --in-place=.bak s*/tmp*/mnt/mysql/tmp* /etc/mysql/my.cnf
 ln -s /mnt/mysql/log /var/log/mysql
 ln -s /mnt/mysql/lib /var/lib/mysql
@@ -34,13 +36,28 @@ apt-get -y upgrade
 /etc/mercury/config_mem.sh
 
 # Set up postfix
-echo hostname > /etc/mailname
-postconf -e 'myhostname = hostname'
-postconf -e 'mydomain = hostname'
-postconf -e 'mydestination = hostname, localhost'
+if [[-a /usr/local/bin/ec2-metadata ]]; then
+    REAL_HOSTNAME=$(/usr/local/bin/ec2-metadata -p | sed 's/public-hostname: //')
+    
+    # Phone home
+    AMI=$(/usr/local/bin/ec2-metadata -a | sed 's/ami-id: //')
+    INSTANCE=$(/usr/local/bin/ec2-metadata -i | sed 's/instance-id: //')
+    curl "http://getpantheon.com/pantheon.php?ami=$AMI&instance=$INSTANCE"
+else
+    REAL_HOSTNAME=`hostname`
+fi
+
+echo $REAL_HOSTNAME > /etc/mailname
+postconf -e "myhostname = ${REAL_HOSTNAME}"
+postconf -e "mydomain = ${REAL_HOSTNAME}"
+postconf -e "mydestination = ${REAL_HOSTNAME}, localhost"
+/etc/init.d/postfix restart
     
 # Reset Drupal Admin Account
 echo "DELETE FROM users WHERE uid = 1;ALTER TABLE users AUTO_INCREMENT = 1;" | mysql -u root -D pressflow
+
+# Unset ssh key gen:
+chmod -x /etc/init.d/ec2-ssh-host-key-gen
 
 # Mark incep date
 echo `date` > /etc/mercury/incep
