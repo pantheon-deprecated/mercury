40b70d52d3bff92def2355add7f01fbc9682e184
Greg Coit <greg@chapterthree.com>
2010-03-01 21:36:31 +0000

    fixed bug in phone home script and added updated CHANGELOG

--- bcfg2/Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
+++ bcfg2/Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
@@ -7,6 +7,5 @@
 
 # Phone home - helps us to know how many users there are without passing any 
 # identifying or personal information to us.
-ID=`hostname | md5sum`
+ID=`hostname | md5sum | sed 's/[^a-zA-Z0-9]//g'`
 curl "http://getpantheon.com/pantheon.php?id=$ID&product=mercury"
-
