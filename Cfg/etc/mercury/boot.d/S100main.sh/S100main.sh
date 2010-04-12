1f290c4f7c33350bc17cc5d89450006c03ce9d1d
Greg Coit <greg@chapterthree.com>
2010-04-12 22:44:06 +0000

    changed hostname to hostname -f for uniqueness

--- Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
+++ Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
@@ -4,5 +4,5 @@
 
 # Phone home - helps us to know how many users there are without passing any 
 # identifying or personal information to us.
-ID=`hostname | md5sum | sed 's/[^a-zA-Z0-9]//g'`
+ID=`hostname -f | md5sum | sed 's/[^a-zA-Z0-9]//g'`
 curl "http://getpantheon.com/pantheon.php?id=$ID&product=mercury"
