3a8c158bf2d7b5fd23c696494ed48987fa153385
Greg Coit <greg@chapterthree.com>
2010-04-12 22:28:04 +0000

    removed config_mem.sh call

--- Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
+++ Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
@@ -2,9 +2,6 @@
 
 # Main/Global Boot Script
 
-# Config Memory
-/etc/mercury/config_mem.sh
-
 # Phone home - helps us to know how many users there are without passing any 
 # identifying or personal information to us.
 ID=`hostname | md5sum | sed 's/[^a-zA-Z0-9]//g'`
