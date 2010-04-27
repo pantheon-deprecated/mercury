1a1e0c684bb97430ae393d8719c7fe0432a006d8
Greg Coit <greg@chapterthree.com>
2010-04-27 15:51:57 +0000

    moved /root/MERCURY_CONFIG to /etc/mercury/MERCURY_TEMPLATE and edited /etc/mercury/boot.d/S100main.sh to copy to /root/MERCURY_TUNEABLES during init

--- Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
+++ Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
@@ -2,6 +2,10 @@
 
 # Main/Global Boot Script
 
+#copy template to root
+cp /etc/mercury/MERCURY_TEMPLATE /root/MERCURY_TUNEABLES
+chown 755 /root/MERCURY_TUNEABLES
+
 # Phone home - helps us to know how many users there are without passing any 
 # identifying or personal information to us.
 ID=`hostname -f | md5sum | sed 's/[^a-zA-Z0-9]//g'`
