724dbeed152506f8d4600eed5bb13e9c4bcf5228
Greg Coit <greg@chapterthree.com>
2010-04-29 00:19:35 +0000

    added braces to if statements to make -1 work

--- Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
+++ Cfg/etc/mercury/boot.d/S100main.sh/S100main.sh
@@ -7,7 +7,7 @@ cp /etc/mercury/template /etc/mercury/server_tuneables
 chown 755 /etc/mercury/server_tuneables
 
 # Postfix
-if [ -a /usr/local/bin/ec2-metadata ]; then
+if [[ -a /usr/local/bin/ec2-metadata ]]; then
     REAL_HOSTNAME=$(/usr/local/bin/ec2-metadata -p | sed 's/public-hostname: //')
 else
     REAL_HOSTNAME=`hostname`
@@ -18,6 +18,10 @@ postconf -e "myhostname = ${REAL_HOSTNAME}"
 postconf -e "mydomain = ${REAL_HOSTNAME}"
 postconf -e "mydestination = ${REAL_HOSTNAME}, localhost"
 /etc/init.d/postfix restart
+
+# MYSQL:
+
+echo "create database pressflow;" | mysql -u root
 
 # Phone home - helps us to know how many users there are without passing any 
 # identifying or personal information to us.
