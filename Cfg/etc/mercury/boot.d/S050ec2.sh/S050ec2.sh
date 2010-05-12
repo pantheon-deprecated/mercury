28ca051fa7be1145ce39cce6f5e931296f43ce5d
Greg Coit <greg@chapterthree.com>
2010-05-12 20:51:10 +0000

    reverting to original file

--- Cfg/etc/mercury/boot.d/S050ec2.sh/S050ec2.sh
+++ Cfg/etc/mercury/boot.d/S050ec2.sh/S050ec2.sh
@@ -3,9 +3,8 @@
 # Move mysql and varnish to /mnt
 # TODO support for EBS and RDS
 
-exit 0;
-
 # Mysql:
+service mysql stop
 mv /var/log/mysql /mnt/mysql/log
 ln -s /mnt/mysql/log /var/log/mysql
 mv /var/lib/mysql /mnt/mysql/lib
