85dc1e1d484987cc432be7c640f6fddb81b77c26
root <root@aaron-lucid>
2010-08-03 20:28:32 +0000

    change initialize and site import to use correct settings template

--- fabric/initialization.py
+++ fabric/initialization.py
@@ -112,7 +112,7 @@ def _initialize_pressflow():
     sudo('mkdir -p /var/www/dev/sites/default/files')
     sudo('touch /var/www/dev/sites/default/files/gitignore')
     sudo('cp /var/www/dev/sites/default/default.settings.php /var/www/dev/sites/default/settings.php')
-    sudo('cat /opt/pantheon/fabric/templates/pantheon.settings.php >> /var/www/dev/sites/default/settings.php')
+    sudo('cat /opt/pantheon/fabric/templates/newsite.settings.php >> /var/www/dev/sites/default/settings.php')
     sudo('mkdir /var/www/live')
     with cd('/var/www/dev'):
         sudo('git init')
