814afaca17cf198241fb8261db7d37e82cd05fbd
andy <github@anarkystic.com>
2010-08-18 16:57:06 +0200

    add import_siteurl to our fabfile

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -2,5 +2,6 @@ from fabric.api import env
 from initialization import initialize, init
 from site_export import export_site
 from site_import import import_site
+from site_import import import_siteurl
 
 env.hosts = ['pantheon@localhost']
