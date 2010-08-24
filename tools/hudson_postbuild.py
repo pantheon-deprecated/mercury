1407a22952154181370a07494875ed938200a779
Aaron Levy <aaron@chapterthree.com>
2010-08-23 19:36:11 -0700

    use originating uuid on return response

--- tools/hudson_postbuild.py
+++ tools/hudson_postbuild.py
@@ -1,7 +1,6 @@
 import json
 import os
 import urllib2
-import uuid
 
 def get_build_status(job, id):
     try:
@@ -18,10 +17,10 @@ if __name__ == '__main__':
     results = dict([(k.lower(), v) for k, v in os.environ.iteritems()])
     results['build_status'] = get_build_status(results.get('job_name'), results.get('build_number'))
 
-    response_keys = ['reply_id', 'build_status', 'job_name', 'build_number','project', 'environment']
+    response_keys = ['uuid', 'build_status', 'job_name', 'build_number','project', 'environment']
     responsebody = dict([(k, v) for k, v in results.iteritems() if k in response_keys])
 
-    responsedict = {'uuid': uuid.uuid4().hex,
+    responsedict = {'uuid': results['uuid'],
                     'command': 'request',
                     'method': 'POST',
                     'url': results['callback_url'],
