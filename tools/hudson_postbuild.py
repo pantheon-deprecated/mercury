282a7ba8fecd9f33bf488957a1839e74cd3fbb11
Josh Koenig <josh@chapterthree.com>
2010-08-24 07:50:17 -0700

    update to unify response spec

--- tools/hudson_postbuild.py
+++ tools/hudson_postbuild.py
@@ -1,6 +1,8 @@
 import json
 import os
 import urllib2
+import uuid
+
 
 def get_build_status(job, id):
     try:
@@ -17,14 +19,14 @@ if __name__ == '__main__':
     results = dict([(k.lower(), v) for k, v in os.environ.iteritems()])
     results['build_status'] = get_build_status(results.get('job_name'), results.get('build_number'))
 
-    response_keys = ['uuid', 'build_status', 'job_name', 'build_number','project', 'environment']
+    response_keys = ['build_status', 'job_name', 'build_number','project', 'environment']
     responsebody = dict([(k, v) for k, v in results.iteritems() if k in response_keys])
 
-    responsedict = {'uuid': results['uuid'],
+    responsedict = {'uuid': uuid.uuid4().hex,
                     'command': 'request',
                     'method': 'POST',
                     'url': results['callback_url'],
-                    'body': responsebody,
+                    'body': {'response': responsebody, 'response_to': {'uuid': results['uuid']}},
                    }
 
     req = urllib2.Request("http://%s/%s" % (host, tube), \
