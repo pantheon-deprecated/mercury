5a1e04e57685f77bbbc5ac446fd4f733b25b3ac6
Aaron Levy <aaron@chapterthree.com>
2010-09-02 23:59:33 +0000

    add restore to fabfile

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -3,5 +3,6 @@ from configure import *
 from initialization import *
 from site_export import *
 from site_import import *
+from site_restore import *
 from update import *
 env.hosts = ['pantheon@localhost']
