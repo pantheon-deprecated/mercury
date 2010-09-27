06d5fc2b90fca9781a97a58ff028d3fa26f48b74
Greg Coit <greg@greg-desktop>
2010-09-27 15:32:16 -0700

    moved ldap install/config from install.py to configure.py

--- fabric/configure.py
+++ fabric/configure.py
@@ -103,3 +103,41 @@ def _report():
 
     local('echo "DEAR SYSADMIN: PANTHEON IS READY FOR YOU NOW.  Do not forget the README.txt, CHANGELOG.txt and docs!" | wall')
 
+def build_ldap_client(base_domain = "example.com", require_group = None, server_host = "auth.example.com"):
+    """ Set permissions on project directory, settings.php, and files dir.
+    environments: Optional. List.
+    
+    """
+    if server_host == "auth.example.com":
+        server_host = "auth." + base_domain
+        
+    # If necessary, restrict by group
+    if require_group is not None:
+        #local("sudo echo '-:ALL EXCEPT root sudo (" + require_group + "):ALL' | tee -a /etc/security/access.conf")
+        local("sudo echo 'AllowGroups root sudo " + require_group + "' | tee -a /etc/ssh/sshd_config")
+    else:
+        local("sudo echo 'AllowGroups root sudo' | tee -a /etc/ssh/sshd_config")
+
+    local("sudo /etc/init.d/ssh restart")
+            
+    ldap_domain = _fabuntu_ldap_domain_to_ldap(base_domain)
+            
+    local("sudo apt-get install -y debconf-utils")
+            
+    ldap_auth_conf_template = loader.load('ldap-auth-config.preseed.cfg', cls=TextTemplate)
+    ldap_auth_conf = ldap_auth_conf_template.generate(ldap_domain=ldap_domain, server_host=server_host).render()
+    with NamedTemporaryFile() as temp_file:
+        temp_file.write(ldap_auth_conf)
+        temp_file.seek(0)
+        local("sudo debconf-set-selections " + temp_file.name)
+        
+    local("sudo apt-get install -y libnss-ldap ldap-auth-config")
+        
+    ldap_conf_template = loader.load('ldap.conf', cls=TextTemplate)
+    ldap_conf = ldap_conf_template.generate(ldap_domain=ldap_domain, server_host=server_host).render()
+    with NamedTemporaryFile() as temp_file:
+        temp_file.write(ldap_conf)
+        temp_file.seek(0)
+        local("sudo cp " + temp_file.name + " /etc/ldap.conf")
+
+    local("sudo auth-client-config -t nss -p lac_ldap")
