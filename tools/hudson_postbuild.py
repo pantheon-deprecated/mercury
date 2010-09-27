aea91f3ce072ba2e3c3e98e0e6bd8ef38bdca5f8
David Strauss <david@fourkitchens.com>
2010-09-27 14:55:44 -0700

    Use httplib instead of urllib2 in order to authenticate with a client certificate.

--- tools/hudson_postbuild.py
+++ tools/hudson_postbuild.py
@@ -1,5 +1,6 @@
 import json
 import os
+import httplib
 import urllib2
 import uuid
 
@@ -10,10 +11,12 @@ def get_build_status(job, id):
         return eval(urllib2.urlopen(req).read()).get('result').lower()
     except urllib2.URLError:
         return "unknown"
-         
+
 if __name__ == '__main__':
 
     host = 'jobs.getpantheon.com'
+    certificate = '/etc/pantheon/system.pem'
+    # TODO: Use a new, response-only tube.
     tube = 'rest-in'
 
     results = dict([(k.lower(), v) for k, v in os.environ.iteritems()])
@@ -29,7 +32,13 @@ if __name__ == '__main__':
                     'body': {'response': responsebody, 'response_to': {'uuid': results['uuid']}},
                    }
 
-    req = urllib2.Request("http://%s/%s" % (host, tube), \
-            headers = {'Content-Type': 'application/json'}, \
-            data = json.dumps(responsedict))
-    urllib2.urlopen(req)
+    # TODO: Use a "with" block for the connection?
+    connection = httplib.HTTPSConnection(
+	host,
+	key_file = certificate,
+	cert_file = certificate
+    )
+    connection.putrequest('POST', '/%s/' % tube)
+    connection.endheaders()
+    connection.send(json.dumps(responsedict))
+    response = connection.getresponse()
