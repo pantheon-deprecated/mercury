2ae792a28cf74bb03c00af6103116531e9e46fdc
Aaron Levy <aaron@getpantheon.com>
2010-10-06 21:07:15 +0000

    changing atlast callback to be part of a generic lib

---
+++ tools/ptools/postback.py
@@ -0,0 +1,36 @@
+import httplib
+import json
+import urllib2
+import uuid
+
+def postback(cargo, command='request', request_uuid=None):
+    """Send data back to Atlas.
+    cargo: dict of data to send.
+    command: Prometheus command.
+    uuid: uuid of requesting job.
+
+    """
+    body = {'response': cargo,
+            'response_to': {'uuid':request_uuid}}
+
+    return _send_response({'uuid': uuid.uuid4().hex,
+                           'command':command,
+                           'method':'POST',
+                           'body':body})
+
+def _send_response(responsedict):
+    """POST data to Prometheus.
+    responsedict: fully formed dict of response data.
+
+    """
+    host = 'jobs.getpantheon.com'
+    certificate = '/etc/pantheon/system.pem'
+    tube = 'atlas-in'
+
+    connection = httplib.HTTPSConnection(host,
+                                         key_file = certificate,
+                                         cert_file = certificate)
+    connection.request('POST', '/%s/' % tube, json.dumps(responsedict))
+    response = connection.getresponse()
+    return response
+
