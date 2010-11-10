1065ea96d7e2da0663518db32416858b28cf7052
Aaron Levy <aaron@chapterthree.com>
2010-11-10 12:15:10 -0800

    add status functions to fabfile

--- fab/fabfile.py
+++ fab/fabfile.py
@@ -3,6 +3,7 @@ from atlas_postback import *
 from configure import *
 from initialization import *
 from ldap import *
+from pantheon.status import *
 from site_backup import *
 from site_onramp import *
 from site_install import *
