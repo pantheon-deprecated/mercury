55befea78c94035d35ab78623b71521c937e3130
Josh Koenig <josh@chapterthree.com>
2010-11-12 09:41:40 -0800

    slightly more flexibility and failure-handling for drupal_update_status support different envs (in theory) report FAILURE rather than blank array() text if choke

--- fab/pantheon/status.py
+++ fab/pantheon/status.py
@@ -12,14 +12,18 @@ def git_repo_status(project):
 
     postback.write_build_data('git_repo_status', {'status': status})
 
-def drupal_update_status(project):
+def drupal_update_status(project, environment='dev'):
     """Return whether or not there's a core update available.
     This will post back directly rather than using a post-build action.
 
     """
-    drushrc = project +'_dev';
+    drushrc = project + '_' + environment;
     with settings(warn_only=True):
-        status = local("drush @%s -n -p upc" % drushrc).rstrip().split('\n')
-
+        status = local("drush @%s -n -p upc" % drushrc)
+    if status.failed:
+        status = 'FAILED'
+    else:
+        status.rstrip().split('\n')
+        
     postback.write_build_data('drupal_update_status', {'status': status})
 
