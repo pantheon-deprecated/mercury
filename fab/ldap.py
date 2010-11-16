3c9c21f51430eed51bca1f7428e81bf444afc9af
David Strauss <david@davidstrauss.net>
2010-11-15 19:28:11 -0800

    Use ssl_cert even for the commented-out code.

--- fab/ldap.py
+++ fab/ldap.py
@@ -51,9 +51,9 @@ def build_ldap_client(base_domain = "example.com", require_group = None, server_
         f.write("\n%" + '%s ALL=(ALL) ALL' % require_group)
 
     # Add LDAP user to www-data, and ssl-cert groups.
-    #local('usermod -aG %s %s' % ('ssl-cert', require_group))
-    # Use sed because usermod may fail if the user does not already exist.
     ssl_group = "ssl-cert"
+    #local('usermod -aG %s %s' % (ssl_group, require_group))
+    # Use sed because usermod may fail if the user does not already exist.
     local('sudo sed -i "s/' + ssl_group + ':x:[0-9]*:/\\0' + require_group + ',/g" /etc/group')
 
     # Restart after ldap is configured so openssh-lpk doesn't choke.
