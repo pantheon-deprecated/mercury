2e07b7a1a58247aeb66f885b4b6c54c395d57a25
Aaron Levy <aaronjlevy@gmail.com>
2010-12-07 14:50:22 -0800

    Add README and simple setup script.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+def deploy():
+    """Install pre-req packages and fabric, then run fab initialize.
+
+    """
+    packages = ['python-configobj',
+                'python-dev',
+                'python-mysqldb',
+                'python-paramiko',
+                'python-pip',
+                'python-crypto',
+                'python-setuptools']
+
+    os.system('apt-get install -y %s' % ' '.join(packages))
+    os.system('pip install fabric')
+    os.system('cd /opt/pantheon/fab && fab initialize')
+
+if __name__ == '__main__':
+    deploy()
+
