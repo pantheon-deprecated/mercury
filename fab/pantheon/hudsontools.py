31acaa517a3fe05e002f5acedccab9c1d2b5e706
Hornswoggles <ray@getpantheon.com>
2011-01-24 20:27:19 +0000

    Added the option to add test cases to a file that has already been written to.

--- fab/pantheon/hudsontools.py
+++ fab/pantheon/hudsontools.py
@@ -2,63 +2,82 @@ import os
 import postback
 
 from xml.dom.minidom import Document
+from xml.dom.minidom import parse
 
-def junit_pass(msg, name):
+def junit_pass(msg, suitename, casename=None):
     """ Create a junit file for a passed test
         msg: The message to add
-        name: Name used for the testsuite, testcase and xml filename
+        suitename: Name used for the testsuite.
+        casename: Name used for the testcase.
     """
-    doc = _base_xml(name)
+    if not casename:
+        casename = suitename
+    doc = _base_xml(suitename)
     testcase = doc.createElement("testcase")
-    testcase.setAttribute("name", "test%s" % name)
+    testcase.setAttribute("name", "test%s" % casename)
     txt = doc.createTextNode(msg)
     testcase.appendChild(txt)
     testsuite = doc.getElementsByTagName('testsuite')[0]
     testsuite.appendChild(testcase)
-    _write_junit_file(doc, name)
+    _write_junit_file(doc, suitename)
 
-def junit_failure(msg, name):
+def junit_failure(msg, suitename, casename=None):
     """ Create a junit file for a failed test
         msg: The message to add
-        name: Name used for the testsuite, testcase and xml filename
+        suitename: Name used for the testsuite.
+        casename: Name used for the testcase.
     """
-    doc = _base_xml(name)
+
+    if not casename:
+        casename = suitename
+    doc = _base_xml(suitename)
     testcase = doc.createElement("testcase")
-    testcase.setAttribute("name", "test%s" % name)
+    testcase.setAttribute("name", "test%s" % casename)
     failure = doc.createElement("failure")
     txt = doc.createTextNode(msg)
     failure.appendChild(txt)
     testcase.appendChild(failure)
     testsuite = doc.getElementsByTagName('testsuite')[0]
     testsuite.appendChild(testcase)
-    _write_junit_file(doc, name)
+    _write_junit_file(doc, suitename)
 
-def junit_error(msg, name):
+def junit_error(msg, suitename, casename=None):
     """ Create a junit file for a error
         msg: The message to add
-        name: Name used for the testsuite, testcase and xml filename
+        suitename: Name used for the testsuite.
+        casename: Name used for the testcase.
     """
-    doc = _base_xml(name)
+
+    if not casename:
+        casename = suitename
+    doc = _base_xml(suitename)
     testcase = doc.createElement("testcase")
-    testcase.setAttribute("name", "test%s" % name)
+    testcase.setAttribute("name", "test%s" % casename)
     error = doc.createElement("error")
     txt = doc.createTextNode(msg)
     error.appendChild(txt)
     testcase.appendChild(error)
     testsuite = doc.getElementsByTagName('testsuite')[0]
     testsuite.appendChild(testcase)
-    _write_junit_file(doc, name)
+    _write_junit_file(doc, suitename)
 
-def _base_xml(name):
+def _base_xml(suitename):
     """ Creates the base xml doc structure
-        name: Name used for the testsuite, testcase and xml filename
+        suitename: Name used for the testsuite.
     """
-    doc = Document()
-    testsuites = doc.createElement("testsuites")
-    doc.appendChild(testsuites)
-    testsuite = doc.createElement("testsuite")
-    testsuite.setAttribute("name", name)
-    testsuites.appendChild(testsuite)
+    try:
+        f = open(os.path.join(postback.get_workspace(), "%s.xml" % 
+                              suitename), 'r')
+    except:
+        doc = Document()
+        testsuites = doc.createElement("testsuites")
+        doc.appendChild(testsuites)
+        testsuite = doc.createElement("testsuite")
+        testsuite.setAttribute("name", suitename)
+        testsuites.appendChild(testsuite)
+    else:
+        doc = parse(f)
+        f.close()
     return doc
 
 def _write_junit_file(doc, filename='results'):
@@ -66,7 +85,8 @@ def _write_junit_file(doc, filename='results'):
         doc: The structured xml document to write to a file
         filename: The filename for the junit report
     """
-    with open(os.path.join(postback.get_workspace(), "%s.xml" % filename), 'w') as f:
+    with open(os.path.join(postback.get_workspace(), "%s.xml" % 
+                           filename), 'w') as f:
         f.write(doc.toprettyxml(indent="  "))
         f.close()
 
