b215f7d016c8a318b499208ae73a47fcad03f783
Hornswoggles <ray@getpantheon.com>
2011-01-24 19:03:33 +0000

    Removing the suppress option. It was not useful

--- fab/atlas_postback.py
+++ fab/atlas_postback.py
@@ -7,11 +7,10 @@ def main():
     usage = "usage: %prog [options]"
     parser = OptionParser(usage=usage, description="Send information about a Hudson job (and resulting data) back to Atlas.")
     parser.add_option('-c', '--check_changed_status', dest="check_changed_status", action="store_true", default=False, help='Postback only if the status of the build has changed from the previous run.')
-    parser.add_option('-s', '--suppress', dest="suppress", action="store_true", default=False, help='Suppress postback on unstable builds.')
     (options, args) = parser.parse_args()
-    postback_atlas(options.check_changed_status, options.suppress)
+    postback_atlas(options.check_changed_status)
 
-def postback_atlas(check_changed_status=False, suppress=False):
+def postback_atlas(check_changed_status=False):
     """ Send information about a Hudson job (and resulting data) back to Atlas.
     check_changed_status: bool. If we want to only return data if the status of
                                 the build has changed from the previous run.
@@ -33,10 +32,7 @@ def postback_atlas(check_changed_status=False, suppress=False):
         response.update({'build_data': postback.get_build_data()})
 
         # Send response to Atlas.
-	if suppress and (response['build_status'] == 'UNSTABLE'):
-	    print('Build status is UNSTABLE. No postback performed.')
-	else:
-            postback.postback(response)
+        postback.postback(response)
     else:
         print('Build status has not changed. No postback performed.')
 
