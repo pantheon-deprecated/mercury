98b774b0f5eb450a60e9316a5de9b5bb78675089
Hornswoggles <ray@getpantheon.com>
2011-01-28 00:19:21 +0000

    Moving vhost logic into pantheon.settings.php

--- fab/templates/drush.alias.drushrc.php
+++ fab/templates/drush.alias.drushrc.php
@@ -1,16 +1,5 @@
 <?php
 
-// Get settings.php variables from vhost file.
-$vhost = explode(PHP_EOL, file_get_contents('${vhost_path}'));
-$vars = array();
-foreach ($vhost as $line) {
-    $line = trim($line);
-    if (strpos($line, 'SetEnv') !== FALSE) {
-        $var = explode(' ', $line);
-        $_SERVER[$var[1]] = $var[2];
-    }
-}
-
 $options['uri'] = 'default';
 $options['root'] = '${root}';
 
--- fab/templates/pantheon.settings.php
+++ fab/templates/pantheon.settings.php
@@ -1,6 +1,47 @@
 <?php
 
-$db_url = "mysqli://$_SERVER[db_username]:$_SERVER[db_password]@localhost/$_SERVER[db_name]";
+$vhostdir = '/etc/apache2/sites-available/';
+
+if ($_SERVER['db_name']) {
+    $db_url = "mysqli://$_SERVER[db_username]:$_SERVER[db_password]@localhost/$_SERVER[db_name]";
+}
+elseif (is_file($vhostdir .'000_pantheon_live') ||
+        is_file($vhostdir .'pantheon_dev') ||
+        is_file($vhostdir .'pantheon_test')) {
+
+    // Determine wether or not drupal_root was specified as a cli option
+    if(drush_get_option('root')) {
+        $drupal_root = drush_get_option('root');
+    }
+    elseif (drush_get_option('r')) {
+        $drupal_root = drush_get_option('r');
+    }
+    else {
+        $drupal_root = $_SERVER['PWD'];
+    }
+
+    // Get settings.php variables from vhost file.
+    if (preg_match('/pantheon\/live/', $drupal_root)) {
+        $vhost = explode(PHP_EOL, file_get_contents($vhostdir .'000_pantheon_live'));
+    }
+    elseif (preg_match('/pantheon\/dev/', $drupal_root)) {
+        $vhost = explode(PHP_EOL, file_get_contents($vhostdir .'pantheon_dev'));
+    }
+    elseif (preg_match('/pantheon\/test/', $drupal_root)) {
+        $vhost = explode(PHP_EOL, file_get_contents($vhostdir .'pantheon_test'));
+    }
+    if ($vhost) {
+        $vars = array();
+        foreach ($vhost as $line) {
+            $line = trim($line);
+            if (strpos($line, 'SetEnv') !== FALSE) {
+                $var = explode(' ', $line);
+                $_SERVER[$var[1]] = $var[2];
+            }
+        }
+        $db_url = "mysqli://$_SERVER[db_username]:$_SERVER[db_password]@localhost/$_SERVER[db_name]";
+    }
+}
 
 $conf['pressflow_smart_start'] = TRUE;
 
