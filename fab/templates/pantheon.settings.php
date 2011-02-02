cb732740db1a8fa3730431c9e87e7afa5ab453e8
Hornswoggles <ray@getpantheon.com>
2011-02-02 14:00:52 -0800

    Fixing tabs to php style

--- fab/templates/pantheon.settings.php
+++ fab/templates/pantheon.settings.php
@@ -3,44 +3,44 @@
 $vhost_dir = '${vhost_root}';
 
 if (isset($_SERVER['db_name'])) {
-    $db_url = "mysqli://$_SERVER[db_username]:$_SERVER[db_password]@localhost/$_SERVER[db_name]";
+  $db_url = "mysqli://$_SERVER[db_username]:$_SERVER[db_password]@localhost/$_SERVER[db_name]";
 }
 elseif (is_file($vhost_dir .'000_${project}_live') ||
         is_file($vhost_dir .'${project}_dev') ||
         is_file($vhost_dir .'${project}_test')) {
 
-    // Determine wether or not drupal_root was specified as a cli option
-    if(drush_get_option('root')) {
-        $drupal_root = drush_get_option('root');
-    }
-    elseif (drush_get_option('r')) {
-        $drupal_root = drush_get_option('r');
-    }
-    else {
-        $drupal_root = $_SERVER['PWD'];
-    }
+  // Determine wether or not drupal_root was specified as a cli option
+  if(drush_get_option('root')) {
+    $drupal_root = drush_get_option('root');
+  }
+  elseif (drush_get_option('r')) {
+    $drupal_root = drush_get_option('r');
+  }
+  else {
+    $drupal_root = $_SERVER['PWD'];
+  }
 
-    // Get settings.php variables from vhost file.
-    if (preg_match('/${project}\/live/', $drupal_root)) {
-        $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'000_${project}_live'));
+  // Get settings.php variables from vhost file.
+  if (preg_match('/${project}\/live/', $drupal_root)) {
+    $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'000_${project}_live'));
+  }
+  elseif (preg_match('/${project}\/dev/', $drupal_root)) {
+    $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'${project}_dev'));
+  }
+  elseif (preg_match('/${project}\/test/', $drupal_root)) {
+    $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'${project}_test'));
+  }
+  if (isset($vhost)) {
+    $vars = array();
+    foreach ($vhost as $line) {
+      $line = trim($line);
+      if (strpos($line, 'SetEnv') !== FALSE) {
+        $var = explode(' ', $line);
+        $_SERVER[$var[1]] = $var[2];
+      }
     }
-    elseif (preg_match('/${project}\/dev/', $drupal_root)) {
-        $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'${project}_dev'));
-    }
-    elseif (preg_match('/${project}\/test/', $drupal_root)) {
-        $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'${project}_test'));
-    }
-    if (isset($vhost)) {
-        $vars = array();
-        foreach ($vhost as $line) {
-            $line = trim($line);
-            if (strpos($line, 'SetEnv') !== FALSE) {
-                $var = explode(' ', $line);
-                $_SERVER[$var[1]] = $var[2];
-            }
-        }
-        $db_url = "mysqli://$_SERVER[db_username]:$_SERVER[db_password]@localhost/$_SERVER[db_name]";
-    }
+    $db_url = "mysqli://$_SERVER[db_username]:$_SERVER[db_password]@localhost/$_SERVER[db_name]";
+  }
 }
 
 $conf['pressflow_smart_start'] = TRUE;
