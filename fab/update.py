20e20b7c443e5709670352419e4d304619da168f
Hornswoggles <ray@getpantheon.com>
2011-02-03 12:51:47 -0800

    Accidently overwrote previous fix to the comment.

--- fab/update.py
+++ fab/update.py
@@ -177,7 +177,7 @@ def update_data(project, environment, source_env, updatedb='True'):
         # updatedb is passed in as a string so we have to evaluate it
         if eval(string.capitalize(updatedb)):
             updater.drupal_updatedb()
-        # The server has a 2 min delay before updates are processed.
+        # The server has a 2min delay before updates to the index are processed
         local("drush @%s_%s solr-reindex" % (project, environment))
         local("drush @%s_%s cron" % (project, environment))
     except:
