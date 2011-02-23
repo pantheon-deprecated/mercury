c8b4cc3aec93fba80aaedb6e0ce9c70d5016eaa1
Hornswoggles <ray@getpantheon.com>
2011-02-23 23:43:50 +0000

    Logging status jobs with syslog

--- fab/monitoring.py
+++ fab/monitoring.py
@@ -5,26 +5,37 @@ import smtplib
 import socket
 import urllib
 import traceback
+import syslog
 
 from fabric.api import *
-from pantheon import hudsontools
 
+syslog.openlog('monitoring')
 
 def check_load_average(limit):
     loads = os.getloadavg()
     if (float(loads[0]) > float(limit)):
-        hudsontools.junit_fail('Load average is %s which is above the threshold of %s.' % (str(loads[0]), str(limit)), 'LoadAverage')
+        syslog.syslog(syslog.LOG_ERR, 'Load average is %s which is above the' \
+                                      ' threshold of %s.' % 
+                                      (str(loads[0]), str(limit)))
     else:
-        hudsontools.junit_pass('Load average is %s which is below the threshold of %s.' % (str(loads[0]), str(limit)), 'LoadAverage')
+        syslog.syslog(syslog.LOG_INFO, 'Load average is %s which is below' \
+                                       ' the threshold of %s.' % 
+                                       (str(loads[0]), str(limit)))
 
 
 def check_disk_space(filesystem, limit):
     s = os.statvfs(filesystem)
     usage = (s.f_blocks - s.f_bavail)/float(s.f_blocks) * 100
     if (float(usage) > float(limit)):
-        hudsontools.junit_fail('Disk usage of %s is at %s percent which is above the threshold of %s percent.' % (filesystem, str(usage), str(limit)), 'DiskSpace') 
+        syslog.syslog(syslog.LOG_ERR, 'Disk usage of %s is at %s percent ' \
+                                      'which is above the threshold of %s ' \
+                                      'percent.' % 
+                                      (filesystem, str(usage), str(limit)))
     else:
-        hudsontools.junit_pass('Disk usage of %s is at %s percent which is above the threshold of %s percent.' % (filesystem, str(usage), str(limit)), 'DiskSpace')
+        syslog.syslog(syslog.LOG_INFO, 'Disk usage of %s is at %s percent' \
+                                       ' which is above the threshold of %s' \
+                                       ' percent.' % 
+                                       (filesystem, str(usage), str(limit)))
 
 
 def check_swap_usage(limit):
@@ -32,17 +43,25 @@ def check_swap_usage(limit):
     swap_used = local("free | grep -i swap | awk '{print $3}'")
     usage = float(swap_used)/float(swap_total) * 100
     if (usage > float(limit)):
-        hudsontools.junit_fail('Swap usage is a %s percent which is above the threshold of %s percent.' % (str(usage), str(limit)), 'SwapUsage')
+        syslog.syslog(syslog.LOG_ERR, 'Swap usage is a %s percent which is' \
+                                      ' above the threshold of %s percent.' % 
+                                      (str(usage), str(limit)))
     else:
-        hudsontools.junit_pass('Swap usage is a %s percent which is below the threshold of %s percent.' % (str(usage), str(limit)), 'SwapUsage')
+        syslog.syslog(syslog.LOG_INFO, 'Swap usage is a %s percent which is ' \
+                                       'below the threshold of %s percent.' % 
+                                       (str(usage), str(limit)))
 
 
 def check_io_wait_time(limit):
     iowait = local("vmstat | grep -v [a-z] | awk '{print $16}'").rstrip()
     if (float(iowait) > float(limit)):
-        hudsontools.junit_fail('IO wait times are at %s percent which is above the threshold of %s percent.' % (str(iowait), str(limit)), 'IOWaitTime')
+        syslog.syslog(syslog.LOG_ERR, 'IO wait times are at %s percent which ' \
+                                      'is above the threshold of %s percent.' % 
+                                      (str(iowait), str(limit)), 'IOWaitTime')
     else:
-        hudsontools.junit_pass('IO wait times are at %s percent which is below the threshold of %s percent.' % (str(iowait), str(limit)), 'IOWaitTime')
+        syslog.syslog(syslog.LOG_INFO, 'IO wait times are at %s percent ' \
+                                       'which is below the threshold of %s ' \
+                                       'percent.' % (str(iowait), str(limit)))
 
 
 def check_mysql(slow_query_limit, memory_usage, innodb_memory_usage, threads):
@@ -50,51 +69,69 @@ def check_mysql(slow_query_limit, memory_usage, innodb_memory_usage, threads):
         messages = list()
         report = local('mysqlreport')
         if report.failed:
-            hudsontools.junit_fail('mysql server does not appear to be running: %s' % report, 'MYSQL')
+            syslog.syslog(syslog.LOG_ERR, 'mysql server does not appear to ' \
+                                          'be running: %s' % report)
         for line in report.splitlines():
             #check for slow wait times:
             if ('Slow' in line and 'Log' in line):
                 if (float(line.split()[5]) > float(slow_query_limit)):
-                    messages.append('MYSQL slow queries is %s percent which is above the threshold of %s percent.' % (line.split()[5], str(slow_query_limit)))
+                    messages.append('MYSQL slow queries is %s percent which ' \
+                                    'is above the threshold of %s percent.' % 
+                                    (line.split()[5], str(slow_query_limit)))
                 else:
-                    messages.append('MYSQL slow queries is %s percent which is below the threshold of %s percent.' % (line.split()[5], str(slow_query_limit)))
+                    messages.append('MYSQL slow queries is %s percent which ' \
+                                    'is below the threshold of %s percent.' % 
+                                    (line.split()[5], str(slow_query_limit)))
 
             #check overall memory usage
             elif ('Memory usage' in line):
                 if (float(line.split()[6]) > float(memory_usage)):
-                    messages.append('MYSQL memory usage is %s percent which is above the threshold of %s percent.' % (line.split()[6], str(memory_usage)))
+                    messages.append('MYSQL memory usage is %s percent which ' \
+                                    'is above the threshold of %s percent.' % 
+                                    (line.split()[6], str(memory_usage)))
                 else:
-                    messages.append('MYSQL memory usage is %s percent which is below the threshold of %s percent.' % (line.split()[6], str(memory_usage)))
+                    messages.append('MYSQL memory usage is %s percent which ' \
+                                    'is below the threshold of %s percent.' % 
+                                    (line.split()[6], str(memory_usage)))
 
             #check InnoDB memory usage
             elif ('Usage' in line and 'Used' in line):
                 if (float(line.split()[5]) > float(innodb_memory_usage)):
-                    messages.append('InnoDB memory usage is %s percent which is above the threshold of %s percent.' % (line.split()[5], str(innodb_memory_usage)))
+                    messages.append('InnoDB memory usage is %s percent which' \
+                                    ' is above the threshold of %s percent.' % 
+                                    (line.split()[5], str(innodb_memory_usage)))
                 else:
-                    messages.append('InnoDB memory usage is %s percent which is below the threshold of %s percent.' % (line.split()[5], str(innodb_memory_usage)))
+                    messages.append('InnoDB memory usage is %s percent which' \
+                                    ' is below the threshold of %s percent.' % 
+                                    (line.split()[5], str(innodb_memory_usage)))
 
             #check thread usage
             elif ('Max used' in line):
                 if (float(line.split()[6]) > float(threads)):
-                    messages.append('Thread usage is %s percent which is above the threshold of %s percent.' % (line.split()[6], str(threads)))
+                    messages.append('Thread usage is %s percent which is ' \
+                                    'above the threshold of %s percent.' % 
+                                    (line.split()[6], str(threads)))
                 else:
-                    messages.append('Thread usage is %s percent which is below the threshold of %s percent.' % (line.split()[6], str(threads)))
+                    messages.append('Thread usage is %s percent which is ' \
+                                    'below the threshold of %s percent.' % 
+                                    (line.split()[6], str(threads)))
                
         message = ' '.join(messages)
         if 'above' in message: 
-            hudsontools.junit_fail(message, 'MYSQLStatus')
+            syslog.syslog(syslog.LOG_ERR, message)
         else:
-            hudsontools.junit_pass(message, 'MYSQLStatus')
+            syslog.syslog(syslog.LOG_INFO, message)
 
 
 def check_ldap():
     try:
         local('ldapsearch -H ldap://auth.getpantheon.com -x -ZZ')
-        hudsontools.junit_pass('ldap responded', 'LDAPStatus')
     except:
-        hudsontools.junit_error('Cannot connect to LDAP on localhost. \n%s' %
-				(traceback.format_exc()), 'LDAPStatus')
+        syslog.syslog(syslog.LOG_ERR, 'Cannot connect to LDAP on localhost.')
+        print(traceback.format_exc())
         raise
+    else:
+        syslog.syslog(syslog.LOG_INFO, 'ldap responded')
 
 
 def check_apache(url):
@@ -115,12 +152,13 @@ def check_pound_via_socket(port):
         port = int(port)
         s.connect(('localhost', port))
         s.shutdown(2)
-        hudsontools.junit_pass('pound responded', 'PoundSocket')
     except:
-        hudsontools.junit_error('Cannot connect to Pound on %s at %s. \n%s' % 
-			        ('localhost', str(port), 
-				 traceback.format_exc()), 'PoundSocket')
+        syslog.syslog(syslog.LOG_ERR, 'Cannot connect to Pound on %s at %s.' % 
+                                      ('localhost', str(port)))
+        print(traceback.format_exc())
         raise
+    else:
+        syslog.syslog(syslog.LOG_INFO, 'pound responded')
 
 
 def check_memcached(port):
@@ -129,21 +167,23 @@ def check_memcached(port):
         port = int(port)
         s.connect(('localhost', port))
         s.shutdown(2)
-        hudsontools.junit_pass('memcached responded', 'MemcachedStatus')
     except:
-        hudsontools.junit_error('Cannot connect to Memcached on %s %s. \n%s' % 
-				('localhost', str(port), 
-				 traceback.format_exc()), 'MemcachedStatus')
+        syslog.syslog(syslog.LOG_ERRA, 'Cannot connect to Memcached on %s ' \
+                                       '%s.' % ('localhost', str(port)))
+        print(traceback.format_exc())
         raise
+    else:
+        syslog.syslog(syslog.LOG_INFO, 'memcached responded')
 
 
 def _test_url(service, url):
-    connection = urllib.urlopen(url)
-    status = connection.getcode()
+    status = urllib.urlopen(url).code
     if (status >=  400):
-        hudsontools.junit_fail('%s returned an error code of %s.' % (service, status), '%sStatus' % service)
+        syslog.syslog(syslog.LOG_ERR, '%s returned an error code of %s.' % 
+                                      (service, status))
     else:
-        hudsontools.junit_pass('%s returned an error code of %s.' % (service, status), '%sStatus' % service)
+        syslog.syslog(syslog.LOG_INFO, '%s returned an error code of %s.' % 
+                                       (service, status))
 # TODO: figure out what to search for from the following output
 #    print(connection.info())
 #    print(connection.read())
