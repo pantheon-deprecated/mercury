b7b736825d34863441ae3207f5bb2a51d7779a02
Hornswoggles <ray@getpantheon.com>
2011-02-24 18:23:31 +0000

    Switched to using the logging module since its more flexible on how logging is to be captured

--- fab/monitoring.py
+++ fab/monitoring.py
@@ -5,68 +5,76 @@ import smtplib
 import socket
 import urllib
 import traceback
-import syslog
+import logging
+import logging.handlers
 
 from fabric.api import *
 
-syslog.openlog('monitoring')
+logger = logging.getLogger('health')
+
+LOG_FILENAME = '/var/log/health.log'
+fh = logging.FileHandler(LOG_FILENAME)
+fh.setLevel(logging.INFO)
+basefmt = logging.Formatter('[%(levelname)s] %(funcName)s %(message)s')
+fh.setFormatter(basefmt)
+logger.addHandler(fh)
+
+''' Log warnings or higher to syslog
+sh = logging.handlers.SysLogHandler('/dev/log')
+sh.setLevel(logging.WARNING)
+sysfmt = logging.Formatter("%(name)s: %(message)s")
+sh.setFormatter(sysfmt)
+logger.addHandler(sh)
+'''
 
 def check_load_average(limit):
     loads = os.getloadavg()
     if (float(loads[0]) > float(limit)):
-        syslog.syslog(syslog.LOG_ERR, 'Load average is %s which is above the' \
-                                      ' threshold of %s.' % 
-                                      (str(loads[0]), str(limit)))
+        logger.warning('Load average is %s which is above the threshold of ' \
+                       '%s.' % (str(loads[0]), str(limit)))
     else:
-        syslog.syslog(syslog.LOG_INFO, 'Load average is %s which is below' \
-                                       ' the threshold of %s.' % 
-                                       (str(loads[0]), str(limit)))
+        logger.info('Load average is %s which is below the threshold of %s.' % 
+                    (str(loads[0]), str(limit)))
 
 def check_disk_space(filesystem, limit):
     s = os.statvfs(filesystem)
     usage = (s.f_blocks - s.f_bavail)/float(s.f_blocks) * 100
     if (float(usage) > float(limit)):
-        syslog.syslog(syslog.LOG_ERR, 'Disk usage of %s is at %s percent ' \
-                                      'which is above the threshold of %s ' \
-                                      'percent.' % 
-                                      (filesystem, str(usage), str(limit)))
+        logger.warning('Disk usage of %s is at %s percent which is above ' \
+                       'the threshold of %s percent.' % 
+                       (filesystem, str(usage), str(limit)))
     else:
-        syslog.syslog(syslog.LOG_INFO, 'Disk usage of %s is at %s percent' \
-                                       ' which is above the threshold of %s' \
-                                       ' percent.' % 
-                                       (filesystem, str(usage), str(limit)))
+        logger.info('Disk usage of %s is at %s percent which is above the ' \
+                    'threshold of %s percent.' % 
+                    (filesystem, str(usage), str(limit)))
 
 def check_swap_usage(limit):
     swap_total = local("free | grep -i swap | awk '{print $2}'")
     swap_used = local("free | grep -i swap | awk '{print $3}'")
     usage = float(swap_used)/float(swap_total) * 100
     if (usage > float(limit)):
-        syslog.syslog(syslog.LOG_ERR, 'Swap usage is a %s percent which is' \
-                                      ' above the threshold of %s percent.' % 
-                                      (str(usage), str(limit)))
+        logger.warning('Swap usage is a %s percent which is above the ' \
+                       'threshold of %s percent.' % (str(usage), str(limit)))
     else:
-        syslog.syslog(syslog.LOG_INFO, 'Swap usage is a %s percent which is ' \
-                                       'below the threshold of %s percent.' % 
-                                       (str(usage), str(limit)))
+        logger.info('Swap usage is a %s percent which is below the ' \
+                    'threshold of %s percent.' % (str(usage), str(limit)))
 
 def check_io_wait_time(limit):
     iowait = local("vmstat | grep -v [a-z] | awk '{print $16}'").rstrip()
     if (float(iowait) > float(limit)):
-        syslog.syslog(syslog.LOG_ERR, 'IO wait times are at %s percent which ' \
-                                      'is above the threshold of %s percent.' % 
-                                      (str(iowait), str(limit)), 'IOWaitTime')
+        logger.warning('IO wait times are at %s percent which is above the ' \
+                       'threshold of %s percent.' % (str(iowait), str(limit)))
     else:
-        syslog.syslog(syslog.LOG_INFO, 'IO wait times are at %s percent ' \
-                                       'which is below the threshold of %s ' \
-                                       'percent.' % (str(iowait), str(limit)))
+        logger.info('IO wait times are at %s percent which is below the ' \
+                    'threshold of %s percent.' % (str(iowait), str(limit)))
 
 def check_mysql(slow_query_limit, memory_usage, innodb_memory_usage, threads):
     with settings(warn_only=True):
         messages = list()
         report = local('mysqlreport')
         if report.failed:
-            syslog.syslog(syslog.LOG_ERR, 'mysql server does not appear to ' \
-                                          'be running: %s' % report)
+            logger.warning('mysql server does not appear to be running: %s' % 
+                           report)
         else:
           for line in report.splitlines():
               #check for slow wait times:
@@ -101,12 +109,14 @@ def check_mysql(slow_query_limit, memory_usage, innodb_memory_usage, threads):
                       messages.append('InnoDB memory usage is %s percent ' \
                                       'which is above the threshold of %s ' \
                                       'percent.' % 
-                                      (line.split()[5], str(innodb_memory_usage)))
+                                      (line.split()[5], 
+                                       str(innodb_memory_usage)))
                   else:
                       messages.append('InnoDB memory usage is %s percent ' \
                                       'which is below the threshold of %s ' \
                                       'percent.' % 
-                                      (line.split()[5], str(innodb_memory_usage)))
+                                      (line.split()[5], 
+                                       str(innodb_memory_usage)))
 
               #check thread usage
               elif ('Max used' in line):
@@ -121,19 +131,19 @@ def check_mysql(slow_query_limit, memory_usage, innodb_memory_usage, threads):
                  
           message = ' '.join(messages)
           if 'above' in message: 
-              syslog.syslog(syslog.LOG_ERR, message)
+              logger.warning(message)
           else:
-              syslog.syslog(syslog.LOG_INFO, message)
+              logger.info(message)
 
 def check_ldap():
     try:
         local('ldapsearch -H ldap://auth.getpantheon.com -x -ZZ')
     except:
-        syslog.syslog(syslog.LOG_ERR, 'Cannot connect to LDAP on localhost.')
+        logger.warning('Cannot connect to LDAP on localhost.')
         print(traceback.format_exc())
         raise
     else:
-        syslog.syslog(syslog.LOG_INFO, 'ldap responded')
+        logger.info('ldap responded')
 
 def check_apache(url):
     _test_url('Apache',url)
@@ -151,12 +161,11 @@ def check_pound_via_socket(port):
         s.connect(('localhost', port))
         s.shutdown(2)
     except:
-        syslog.syslog(syslog.LOG_ERR, 'Cannot connect to Pound on %s at %s.' % 
-                                      ('localhost', str(port)))
-        print(traceback.format_exc())
+        logger.warning('Cannot connect to Pound on %s at %s.' % 
+                       ('localhost', str(port), traceback.format_exc()))
         raise
     else:
-        syslog.syslog(syslog.LOG_INFO, 'pound responded')
+        logger.info('pound responded')
 
 def check_memcached(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -165,21 +174,18 @@ def check_memcached(port):
         s.connect(('localhost', port))
         s.shutdown(2)
     except:
-        syslog.syslog(syslog.LOG_ERR, 'Cannot connect to Memcached on %s ' \
-                                       '%s.' % ('localhost', str(port)))
-        print(traceback.format_exc())
+        logger.warning('Cannot connect to Memcached on %s %s. \n%s' % 
+                       ('localhost', str(port), traceback.format_exc()))
         raise
     else:
-        syslog.syslog(syslog.LOG_INFO, 'memcached responded')
+        logger.info('memcached responded')
 
 def _test_url(service, url):
     status = urllib.urlopen(url).code
     if (status >=  400):
-        syslog.syslog(syslog.LOG_ERR, '%s returned an error code of %s.' % 
-                                      (service, status))
+        logger.warning('%s returned an error code of %s.' % (service, status))
     else:
-        syslog.syslog(syslog.LOG_INFO, '%s returned an error code of %s.' % 
-                                       (service, status))
+        logger.info('%s returned an error code of %s.' % (service, status))
 # TODO: figure out what to search for from the following output
 #    print(connection.info())
 #    print(connection.read())
