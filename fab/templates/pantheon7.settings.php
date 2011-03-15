72a5a8e87dbd7592bdb1b640e596e15373799a2f
Aaron Levy <aaron@getpantheon.com>
2011-03-15 12:14:57 -0700

    Create a generic pantheon.settings.php file that does not need template values.

--- fab/templates/pantheon7.settings.php
+++ fab/templates/pantheon7.settings.php
@@ -1,8 +1,38 @@
 <?php
 
-$vhost_dir = '${vhost_root}';
+function get_project_and_environment() {
+  $parts = explode('/', DRUPAL_ROOT);
+  return array_slice($parts, -2, 2);
+}
 
-/* Database settings */
+function get_vhost_file($project, $environment) {
+  $vhost_dir = '/etc/apache2/sites-available/';
+  if ($environment == 'live') {
+    return $vhost_dir . '000_' . $project . '_' .$environment;
+  }
+  else {
+    return $vhost_dir . $project . '_' . $environment;
+  }
+}
+
+if (defined('DRUPAL_ROOT')) {
+  // If DRUPAL_ROOT is set at this point, the request is coming from Drush.
+  list($project, $environment) = get_project_and_environment();
+  $vhost_file = get_vhost_file($project, $environment);
+  $vhost = explode(PHP_EOL, file_get_contents($vhost_file));
+
+  // Populate $_SERVER with values from vhost file.
+  $vars = array();
+  foreach ($vhost as $line) {
+    $line = trim($line);
+    if (strpos($line, 'SetEnv') !== FALSE) {
+      $var = explode(' ', $line);
+      $_SERVER[$var[1]] = $var[2];
+    }
+  }
+}
+
+// $_SERVER should now be populated from a Apache request or parsed from vhost for Drush request.
 if (isset($_SERVER['db_name'])) {
   $databases = array (
     'default' =>
@@ -19,57 +49,6 @@ if (isset($_SERVER['db_name'])) {
       ),
     ),
   );
-}
-elseif (is_file($vhost_dir .'000_${project}_live') ||
-        is_file($vhost_dir .'${project}_dev') ||
-        is_file($vhost_dir .'${project}_test')) {
-
-  // Determine wether or not drupal_root was specified as a cli option.
-  if(drush_get_option('root')) {
-    $drupal_root = drush_get_option('root');
-  }
-  elseif (drush_get_option('r')) {
-    $drupal_root = drush_get_option('r');
-  }
-  else {
-    $drupal_root = $_SERVER['PWD'];
-  }
-
-  // Get settings.php variables from vhost file.
-  if (preg_match('/${project}\/live/', $drupal_root)) {
-    $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'000_${project}_live'));
-  }
-  elseif (preg_match('/${project}\/dev/', $drupal_root)) {
-    $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'${project}_dev'));
-  }
-  elseif (preg_match('/${project}\/test/', $drupal_root)) {
-    $vhost = explode(PHP_EOL, file_get_contents($vhost_dir .'${project}_test'));
-  }
-  if (isset($vhost)) {
-    $vars = array();
-    foreach ($vhost as $line) {
-      $line = trim($line);
-      if (strpos($line, 'SetEnv') !== FALSE) {
-        $var = explode(' ', $line);
-        $_SERVER[$var[1]] = $var[2];
-      }
-    }
-    $databases = array (
-      'default' =>
-      array (
-        'default' =>
-        array (
-          'database' => $_SERVER['db_name'],
-          'username' => $_SERVER['db_username'],
-          'password' =>  $_SERVER['db_password'],
-          'host' => 'localhost',
-          'port' => '',
-          'driver' => 'mysql',
-          'prefix' => '',
-        ),
-      ),
-    );
-  }
 }
 
 $conf['pressflow_smart_start'] = TRUE;
