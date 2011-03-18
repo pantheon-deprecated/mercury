99bfa2dd9aa5347ec0db9ca5b78a252834ca0a8d
Aaron Levy <aaron@getpantheon.com>
2011-03-18 00:17:29 +0000

    Update the dev download process to use specific names. Also get the server name from ygg rather than requiring it as a build param.

--- fab/site_devel.py
+++ fab/site_devel.py
@@ -1,37 +1,34 @@
 import os
 import string
+import traceback
 
 from pantheon import backup
+from pantheon import jenkinstools
 from pantheon import pantheon
 
 DESTINATION = '/srv/dev_downloads'
 
-def get_dev_downloads(resource, archive_name, project, user=None):
+def get_dev_downloads(resource, project, user=None):
     """Wapper method for a Jenkins job to get development resources.
     resource: type of download you want (all/files/data/code/drushrc)
-    archive_name: resulting name of archive.
     project: project name
+    server_name: getpantheon server name.
     user: user that has ssh access to box.
 
     """
-    if resource == 'all':
-        print "Creating archive of all dev resources."
-        _dev_all(archive_name, project, user)
-    elif resource == 'files':
-        print "Creating archive of dev files."
-        _dev_files(archive_name, project)
-    elif resource == 'data':
-        print "Creating archive of dev data."
-        _dev_data(archive_name, project)
-    elif resource == 'code':
-        print "Creating archive of dev code."
-        _dev_code(archive_name, project, user)
-    elif resource == 'drushrc':
-        print "Creating remote drushrc file."
-        destination = os.path.join(_get_destination(),
-                                   '%s.aliases.drushrc' % project)
-        _dev_drushrc(project, user, destination)
-
+    try:
+        archive_name = 'local_dev_%s' % resource
+        resource_handler = {'all': _dev_all,
+                            'files': _dev_files,
+                            'data': _dev_data,
+                            'code': _dev_code,
+                            'drushrc': _dev_drushrc}
+        resource_handler[resource](archive_name, project, user)
+    except:
+        jenkinstools.junit_error(traceback.format_exc(), 'DevDownloads')
+        raise
+    else:
+        jenkinstools.junit_pass('', 'DevDownloads')
 
 def _dev_all(archive_name, project, user):
     archive = backup.PantheonBackup(archive_name, project)
@@ -39,60 +36,31 @@ def _dev_all(archive_name, project, user):
     # Only create archive of development environment data.
     archive.get_dev_files()
     archive.get_dev_data()
-    archive.get_dev_code(user, project)
-
-    # Create a drushrc aliases file.
-    destination = os.path.join(archive.backup_dir,'%s.aliases.drushrc.php' % project)
-    _dev_drushrc(project, user, destination)
+    archive.get_dev_code(user)
+    archive.get_dev_drushrc(user)
 
     # Create the tarball and move to final location.
     archive.finalize(_get_destination())
 
-def _dev_files(archive_name, project):
+def _dev_files(archive_name, project, *args):
     archive = backup.PantheonBackup(archive_name, project)
     archive.get_dev_files()
     archive.finalize(_get_destination())
 
-def _dev_data(archive_name, project):
+def _dev_data(archive_name, project, *args):
     archive = backup.PantheonBackup(archive_name, project)
     archive.get_dev_data()
     archive.finalize(_get_destination())
 
 def _dev_code(archive_name, project, user):
-    #TODO: For now host == project. This may change.
-    host = project
     archive = backup.PantheonBackup(archive_name, project)
-    archive.get_dev_code(user, host)
+    archive.get_dev_code(user)
     archive.finalize(_get_destination())
 
-def _dev_drushrc(project, user, destination):
-    host = '%s.gotpantheon.com' % project
-    environments = pantheon.get_environments()
-
-    # Build the environment specific aliases
-    env_aliases = ''
-    template = string.Template(_get_env_alias())
-
-    for env in environments:
-        values = {'host': host,
-                  'user': user,
-                  'project': project,
-                  'env': env,
-                  'root': '/var/www/%s/%s' % (project, env)}
-        env_aliases += template.safe_substitute(values)
-
-    with open(destination, 'w') as f:
-        f.write('<?php\n%s\n' % env_aliases)
-
-def _get_env_alias():
-    return """
-$aliases['${project}_${env}'] = array(
-  'remote-host' => '${host}',
-  'remote-user' => '${user}',
-  'uri' => 'default',
-  'root' => '${root}',
-);
-"""
+def _dev_drushrc(archive_name, project, user):
+    archive = backup.PantheonBackup(archive_name, project)
+    archive.get_dev_drushrc(user)
+    archive.finalize(_get_destination())
 
 def _get_destination():
     return DESTINATION
