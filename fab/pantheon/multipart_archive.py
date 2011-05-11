8dcd969e8dd7fb8fdf8b981a7be80fd4b898d40a
Hornswoggles <ray@getpantheon.com>
2011-05-11 03:01:12 +0000

    Renaming files

--- fab/pantheon/multipart_archive.py
+++ fab/pantheon/multipart_archive.py
@@ -1,130 +1,157 @@
+import hashlib
+import base64
 import httplib
+import sys
 import os
-from pprint import pprint
+import logging
+import json
+import rangeable_file
 
-class RangeableFileObject():
-    """File object wrapper to enable raw range handling.
-    This was implemented primarilary for handling range 
-    specifications for file:// urls. This object effectively makes 
-    a file object look like it consists only of a range of bytes in 
-    the stream.
-    """
-    
-    def __init__(self, fo, rangetup):
-        """Create a RangeableFileObject.
-        fo       -- a file like object. only the read() method need be 
-                    supported but supporting an optimized seek() is 
-                    preferable.
-        rangetup -- a (firstbyte,lastbyte) tuple specifying the range
-                    to work over.
-        The file object provided is assumed to be at byte offset 0.
-        """
-        self.fo = fo
-        (self.firstbyte, self.lastbyte) = range_tuple_normalize(rangetup)
-        self.realpos = 0
-        self._do_seek(self.firstbyte)
-        
-    def __getattr__(self, name):
-        """This effectively allows us to wrap at the instance level.
-        Any attribute not found in _this_ object will be searched for
-        in self.fo.  This includes methods."""
-        if hasattr(self.fo, name):
-            return getattr(self.fo, name)
-        raise AttributeError, name
+# Set up logging.
+logger = logging.getLogger("Archiver")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
-    def read(self, size=-1):
-        """Read within the range.
-        This method will limit the size read based on the range.
-        """
-        size = self.__len__()
-        rslt = self.fo.read(size)
-        self.realpos += len(rslt)
-        return rslt
-    
-    def __len__(self):
-        """Handles calculating the amount of data to read based on
-        the range.
-        """
-        #return self._calc_read_size(self.chunk_size)
-        return self.lastbyte - self.firstbyte 
+CERTIFICATE = "/etc/pantheon/system.pem"
+API_SERVER = "184.106.214.232"
+ARCHIVE_SERVER = "s3.amazonaws.com"
 
-    def _do_seek(self,offset):
-        """Seek based on whether wrapped object supports seek().
-        offset is relative to the current position (self.realpos).
-        """
-        assert offset >= 0
-        self.fo.seek(self.realpos + offset)
-        self.realpos+= offset
-
-    def seek(self,offset,whence=0):
-        """Seek within the byte range.
-        Positioning is identical to that described under tell().
-        """
-        assert whence in (0, 1, 2)
-        if whence == 0:   # absolute seek
-            realoffset = self.firstbyte + offset
-        elif whence == 1: # relative seek
-            realoffset = self.realpos + offset
-        elif whence == 2: # absolute from end of file
-            # XXX: are we raising the right Error here?
-            raise IOError('seek from end of file not supported.')
-        
-        # do not allow seek past lastbyte in range
-        if self.lastbyte and (realoffset >= self.lastbyte):
-            realoffset = self.lastbyte
-        
-        self._do_seek(realoffset - self.realpos)
-
-def range_tuple_normalize(range_tup):
-    """Normalize a (first_byte,last_byte) range tuple.
-    Return a tuple whose first element is guaranteed to be an int
-    and whose second element will be '' (meaning: the last byte) or 
-    an int. Finally, return None if the normalized tuple == (0,'')
-    as that is equivelant to retrieving the entire file.
-    """
-    if range_tup is None: return None
-    # handle first byte
-    fb = range_tup[0]
-    if fb in (None,''): fb = 0
-    else: fb = int(fb)
-    # handle last byte
-    try: lb = range_tup[1]
-    except IndexError: lb = ''
-    else:  
-        if lb is None: lb = ''
-        elif lb != '': lb = int(lb)
-    # check if range is over the entire file
-    if (fb,lb) == (0,''): return None
-    # check that the range is valid
-    if lb < fb: raise RangeError(9, 'Invalid byte range: %s-%s' % (fb,lb))
-    return (fb,lb)
-
-def fbuffer(fpath, chunk_size):
-    fsize = os.path.getsize(fpath)
-    byte = 0
-    for i in range(fsize/chunk_size):
-        if (fsize > chunk_size):
-            rfo = RangeableFileObject(file(fpath), (byte, byte + chunk_size))
-            yield rfo
-            rfo.close()
-            byte += chunk_size
-    else:
-        rfo = RangeableFileObject(file(fpath), (byte, fsize))
-        yield rfo
-        rfo.close()
-
-filepath = 'test5.txt'
-chunksize = 893
-connection = httplib.HTTPConnection(
-    'www.postbin.org',
+connection = httplib.HTTPSConnection(
+    API_SERVER,
+    8443,
+    key_file = CERTIFICATE,
+    cert_file = CERTIFICATE
 )
 
-#for chunk in fbuffer(filepath, chunksize):
-if True:
-    chunk = open(filepath)
-    connection.connect()
-    connection.request("PUT", '/1jskuz1', chunk)
-    complete_response = connection.getresponse()
-    pprint(complete_response.read())
-    connection.close()
-#    print(chunk.read())
+def main(path):
+    #filename = os.path.basename(path)
+    #upid = initiate_multipart_archive(filename)
+    #partno = 1
+    #parts = []
+    mpa = Multipart_archive(path)
+    for chunk in rangeable_file.fbuffer(path, 2500):
+        partno = mpa.partno
+        mph = mpa.get_multipart_upload_header(chunk)
+        chunk.seek(0)
+        # Transfer the file to long-term storage.
+        arch_connection = httplib.HTTPSConnection(mph['hostname'])
+        arch_connection.request("PUT", mph['path'], chunk, mph['headers'])
+        arch_complete_response = arch_connection.getresponse()
+        if arch_complete_response.status == 200:
+            logger.debug('Successfully pushed the file to the archive.')
+        else:
+            logger.debug('Uploading the file to the archive failed.')
+            exit(2)
+        etag = arch_complete_response.getheader('etag')
+        mpa.parts.append((partno, etag))
+
+    mpa.complete_multipart_upload()
+
+class Multipart_archive:
+    def __init__(self, path):
+        self.path = path
+        self.filename = os.path.basename(path)
+        self.upid = self.initiate_multipart_archive(self.filename)
+        self.partno = 1
+        self.parts = []
+
+    def hash_file(self, part):
+        """ Return MD5 hash of part
+
+        Keyword arguements:
+        part -- the file object to hash
+
+        """
+        part_hash = hashlib.md5()
+        for chunk in iter(lambda: part.read(128*part_hash.block_size), ''):
+            part_hash.update(chunk)
+        return base64.b64encode(part_hash.digest())
+
+
+    def initiate_multipart_archive(self, filename):
+        """ Return the upload id from ygg.
+
+        Keyword arguements:
+        filename -- the name of the file being imported
+
+        """
+        # Get the authorization headers.
+        headers = {'Content-Type': 'application/x-tar',
+                   'multipart': 'initiate'}
+        encoded_headers = json.dumps(headers)
+        connection.request("PUT", "/sites/self/archive/{0}".format(filename), 
+                           encoded_headers)
+        complete_response = connection.getresponse()
+        if complete_response.status == 200:
+            logger.debug('Successfully obtained authorization.')
+        else:
+            logger.debug('Obtaining authorization failed.')
+            exit(1)
+        #complete_response.reason
+        encoded_info = complete_response.read()
+        info = json.loads(encoded_info)
+        connection.close()
+        logger.debug('Return upload id.')
+        return info
+
+    def get_multipart_upload_header(self, part):
+        """ Return the multipart upload headers from ygg.
+
+        Keyword arguements:
+        part -- file object of current chunk
+        partno -- part number
+        upid -- unique upload id
+
+        """
+        # Get the MD5 hash of the file.
+        logger.debug("Archiving file at path: %s" % self.path)
+        part_hash = self.hash_file(part)
+        logger.debug("Hash of file is: %s" % part_hash)
+        connection.connect()
+        headers = {'Content-Type': 'application/x-tar',
+                   'Content-MD5': part_hash,
+                   'multipart': 'upload',
+                   'upload-id': self.upid,
+                   'part-number': self.partno}
+        encoded_headers = json.dumps(headers)
+        connection.request("PUT", "/sites/self/archive/{0}".format(self.filename), encoded_headers)
+        complete_response = connection.getresponse()
+        if complete_response.status == 200:
+            logger.debug('Successfully obtained authorization.')
+        else:
+            logger.debug('Obtaining authorization failed.')
+            exit(1)
+        #complete_response.reason
+        encoded_info = complete_response.read()
+        #connection.close()
+        self.partno+=1
+        return json.loads(encoded_info)
+
+    def complete_multipart_upload(self):
+        # Notify the event system of the completed transfer.
+        connection.connect()
+        headers = {'Content-Type': 'application/x-tar',
+                   'multipart': 'complete',
+                   'upload-id': self.upid,
+                   'parts': self.parts}
+        encoded_headers = json.dumps(headers)
+        connection.request("PUT", "/sites/self/archive/{0}".format(self.filename), 
+                           encoded_headers)
+        complete_response = connection.getresponse()
+        if complete_response.status == 200:
+            logger.debug('Successfully obtained authorization.')
+        else:
+            logger.debug('Obtaining authorization failed.')
+            exit(1)
+        #complete_response.reason
+        encoded_info = complete_response.read()
+        return encoded_info
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+#connection.request("PUT", "/sites/self/archive/{0}/complete".format(filename))
+
