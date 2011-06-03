b8134414c968116a6d44f21e5e4bd358a2419ee9
Aaron Levy <aaron@getpantheon.com>
2011-06-03 15:44:41 -0700

    Remove references to platform in the status check

--- fab/pantheon/drupaltools.py
+++ fab/pantheon/drupaltools.py
@@ -29,25 +29,17 @@ def get_drupal_update_status(project):
         with cd(env_path):
             local('git fetch origin')
 
-            platform = get_drupal_platform(env_path)
             drupal_version = get_drupal_version(env_path)
 
             # python -> json -> php boolean disagreements. Just use int.
             drupal_update = int(latest_drupal_version != drupal_version)
 
-            #TODO: This should be removed once the onramp process has switched
-            # to always using the latest version of pantheon.
-            if platform == 'PANTHEON' or platform == 'PRESSFLOW' or \
-                                         drupal_version[0] == '7':
-                pantheon_log = local('git log refs/heads/%s' % project + \
-                                     '..refs/remotes/origin/master'
-                                    ).rstrip('\n')
-            else:
-                pantheon_log = 'Upgrade to Pressflow/Pantheon'
-
+            # Determine if there have been any new commits.
             # NOTE: Removed reporting back with log entries, so using logs
             # to determine if there is an update is a little silly. However,
             # we may want to send back logs someday, so leaving for now.
+            pantheon_log = local('git log refs/heads/%s' % project + \
+                                 '..refs/remotes/origin/master').rstrip('\n')
 
             # If log is impty, no updates.
             pantheon_update = int(bool(pantheon_log))
@@ -103,28 +95,4 @@ def _parse_drupal_version(location):
     if len(version) > 1 and version[0:1] in ['6', '7']:
         return version
     return None
-
-def _parse_changelog(changelog):
-    """Parse a diff file and return a string of any lines added.
-    changelog: string of diff style output.
-
-    """
-    #TODO: Temporary until we can get this information from a drupal git repo.
-
-    # Parse the diff of the changelog. Only keep lines that were added.
-    log = [line[1:] for line in changelog.split('\n') if (line[0:1] == '+' and
-                                                          line[0:3] not in
-                                                          ['+++', '+//'])]
-    # Remove information about Drupal 5 updates. 
-    ret = list()
-    remove = False
-    for line in log:
-        if line[0:8] == 'Drupal 6':
-            remove = False
-        elif line[0:8] == 'Drupal 5':
-            ret.append('')
-            remove = True
-        if not remove:
-            ret.append(line)
-    return '\n'.join(ret)
 
